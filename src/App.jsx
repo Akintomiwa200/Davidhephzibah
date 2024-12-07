3382e2bcb087700a40f6d9ef5195dc7b7ddeb8eb
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-12-07 11:16:14 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
 import WhatYouLearn from './components/WhatYouLearn';
-//import Testimonial from './components/Testmonial'; 
+import Testimonial from './components/Testmonial'; 
 import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
 import StockMarketMasterClass from './components/StockMarketMasterClass';
@@ -24,7 +24,7 @@ function App() {
         <About />
         <StockMarketMasterClass/>
         <WhatYouLearn />
-      //  <Testimonial />
+      <Testimonial />
         <ContactForm />
         <Footer />
       </div>
