fcfc337541d3f46e5572d832f214bb6f3eeebc37
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-12-07 22:12:47 +0100

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,9 @@ function Footer() {
         <footer className="py-8 bg-blue-500 text-white text-center">
             <p className="mb-4">&copy; 2024 Financial Coach. All rights reserved.</p>
             <div className="flex justify-center space-x-4">
-                 <a href="https://www.facebook.com/share/15Mp863VE3/">
-                                 <FaFacebook className="text-xl" />
-                </a>
-         <a href="https://www.instagram.com/coach_davidhephzibah?igsh=YzljYTk1ODg3Zg==">
-                      <FaInstagram className="text-xl" />
-                    </a>
-             //   <FaLinkedin className="text-xl" />
+                <FaFacebook className="text-xl" />
+                <FaTwitter className="text-xl" />
+                <FaLinkedin className="text-xl" />
             </div>
         </footer>
     );
