029ebb2e71aae452a2096c0c87592b53947d61a5
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-12-08 13:45:57 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         <About />
         <StockMarketMasterClass/>
         <WhatYouLearn />
-      //<Testimonial />
+      /*<Testimonial />*/
         <ContactForm />
         <Footer />
       </div>
