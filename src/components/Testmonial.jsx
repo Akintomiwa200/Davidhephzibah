8b8f4020498d1bd29c662c630705583334d47c75
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-12-14 15:56:47 +0100

    Update Testmonial.jsx

--- src/components/Testmonial.jsx
+++ src/components/Testmonial.jsx
@@ -81,7 +81,11 @@ const TestimonialSlider = () => {
               >
                 <div className="bg-gray-100 p-8 rounded-lg text-left w-full shadow-lg">
                   <FaUser className="w-8 h-8 text-gray-500 mb-4" />
-                  <p className="leading-relaxed mb-6">{testimonial.quote}</p>
+               <p
+                  className="leading-relaxed mb-6 break-words"
+                  style={{ wordWrap: "break-word", overflowWrap: "break-word", whiteSpace: "normal" }}
+                >
+                 {testimonial.quote}</p>
                   <div className="inline-flex items-center">
                     <img
                       alt="testimonial"
